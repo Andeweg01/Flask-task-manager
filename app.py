eb91c37f4d2b1d4cc618e0d0d587863706c703d0
Sascha Andeweg <sascha@designed.irish>
2020-05-09 18:59:02 +0000

    initial commit task_manager application

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "hello world!"
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP', '0.0.0.0'),
+        port=int(os.environ.get('PORT', '5000')),
+        debug=True)
