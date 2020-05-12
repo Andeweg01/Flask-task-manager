f57423b1f2b3ce748543d7982881318a60ae4228
Sascha Andeweg <sascha@designed.irish>
2020-05-12 20:54:52 +0000

    minor changes looking for fixes to errors in heroku

--- app.py
+++ app.py
@@ -12,11 +12,13 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    return render_template("tasks.html", 
+    tasks=mongo.db.tasks.find())
 
 @app.route('/add_task')
 def add_task():
-    return render_template("addtask.html", categories=mongo.db.categories.find())
+    return render_template("addtask.html", 
+    categories=mongo.db.categories.find())
 
 @app.route('/insert_task', methods=['POST'])
 def insert_task():
