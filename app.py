a2f03417e5e50cc64bbf81911282ee185040f1ee
Sascha Andeweg <sascha@designed.irish>
2020-05-13 17:27:22 +0000

    commit of removed localhost from app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
 
 mongo = PyMongo(app)
 
