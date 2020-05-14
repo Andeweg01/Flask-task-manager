cde6ac9fd3e185d5d9ca61423556c0ccc29caead
Sascha Andeweg <sascha@designed.irish>
2020-05-14 16:32:35 +0000

    commit of situation using env.py for environment variables

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from os import path 
+if path.exists("env.py"): 
+    import env
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
